15a4e050a607368e9276f2847ebb5573768fd9e2
Ankit Kumar <ak8415129@gmail.com>
2025-01-08 20:53:26 +0530

    done

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,11 +1,18 @@
 export const exerciseOptions={ 
 	method: 'GET',
 	headers: {
-		 'X-RapidAPI-Key': '2a057a5bd8mshdfe2fedeffa0504p199b83jsn3656b138b053',
-		// 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+		  'X-RapidAPI-Key': '2a057a5bd8mshdfe2fedeffa0504p199b83jsn3656b138b053',
+		//  'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
 		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
 	}
 };
+export const youtubeOptions = {
+	method: 'GET',
+	headers: {
+	  'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+	  'X-RapidAPI-Key': '2a057a5bd8mshdfe2fedeffa0504p199b83jsn3656b138b053',
+	},
+  };
 
 export const fetchData =async (url,options)=>{
 
